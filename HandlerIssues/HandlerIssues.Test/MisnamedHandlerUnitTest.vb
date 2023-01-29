0905ab6b1fee6517daa86b665b1ec9a9738cb262
Paul M Cohen <paul1956@users.noreply.github.com>
2023-01-28 18:22:20 -0800

    Add Headers

--- HandlerIssues/HandlerIssues.Test/MisnamedHandlerUnitTest.vb
+++ HandlerIssues/HandlerIssues.Test/MisnamedHandlerUnitTest.vb
@@ -1,4 +1,8 @@
-﻿Imports Microsoft.CodeAnalysis.Testing
+﻿' Licensed to the .NET Foundation under one or more agreements.
+' The .NET Foundation licenses this file to you under the MIT license.
+' See the LICENSE file in the project root for more information.
+
+Imports Microsoft.CodeAnalysis.Testing
 Imports Microsoft.VisualStudio.TestTools.UnitTesting
 
 Imports VerifyVB = HandlerIssues.Test.VisualBasicCodeFixVerifier(
